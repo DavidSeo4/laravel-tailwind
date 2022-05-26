771e07d255b54821d325d3ed16874ab9be62d816
= <=>
2022-05-26 09:17:09 +0200

    ki

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Auth::routes();
-
-Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::get('/', [\App\Http\Controllers\PostController::class, 'home'])->name('home');
 Route::get('/posts/{slug}', [\App\Http\Controllers\PostController::class, 'detail'])->name('posts.detail');
+Auth::routes();
 
 Route::post('/comment', [\App\Http\Controllers\CommentController::class, 'store'])->name('comments.store');
-
-
 Route::resource('/admin/posts', \App\Http\Controllers\PostController::class);
